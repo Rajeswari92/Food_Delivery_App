0464b893a457471edc103a1d78a4512fed3e2005
Rajeswari92 <raji032019@gmail.com>
2024-12-12 23:39:00 +0530

    Updated full code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://FoodApp:FoodApp@food-delivery-app-db.6u1qm.mongodb.net/?retryWrites=true&w=majority&appName=Food-Delivery-App-DB')
+    await mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected"))
 }
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
-
+import userRouter from './routes/userRoute.js'
+import 'dotenv/config'
+import cartRouter from './routes/cartRoutes.js'
+import orderRouter from './routes/orderRoute.js'
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use(cors())
@@ -18,6 +21,9 @@ connectDB();
 //api endpoint
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.listen(port, ()=>{
     console.log(`server is running on http://localhost:${port}`)
