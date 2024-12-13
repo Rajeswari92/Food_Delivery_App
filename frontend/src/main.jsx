11cc05536ac65fcb9abcacf32495d62cfc1a35f9
Rajeswari Narayanan <121309581+Rajeswari92@users.noreply.github.com>
2024-12-13 22:04:17 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/StoreContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <BrowserRouter
-  future={{
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-  }}
+   <BrowserRouter
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
   >
+    <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+     </BrowserRouter>
 );
