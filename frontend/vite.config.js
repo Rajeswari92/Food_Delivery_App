a02c49b4142039a2e702f693051b709750d975eb
Rajeswari Narayanan <121309581+Rajeswari92@users.noreply.github.com>
2024-12-13 23:23:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/subdirectory/', // Adjust this to your hosting path
-});
+})
+
