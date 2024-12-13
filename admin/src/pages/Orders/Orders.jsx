7b85c97986c255440d5db766b3809dc13c9d7abc
Rajeswari Narayanan <121309581+Rajeswari92@users.noreply.github.com>
2024-12-13 18:50:41 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import "./Orders.css"
 import axios from "axios"
 import { toast } from "react-toastify"
-import { assets } from '../../assets/admin_assets/assets'
+import { assets } from '../../assets/admin_assets/assets.js'
 const Orders = ({url}) => {
 
     const [orders, setOrders] = useState([]);
